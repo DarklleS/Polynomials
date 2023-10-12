8c512471a03e627131b5979b0e75e4f1f089f739
Daniel Pogosov <51363793+DarklleS@users.noreply.github.com>
2023-10-12 17:01:09 +0300

    Update main.cpp

--- polynomials/src/main.cpp
+++ polynomials/src/main.cpp
@@ -1,4 +1,4 @@
-﻿#include "../inc/excerpt.h"
+#include "../inc/excerpt.h"
 #include "../inc/modules.h"
 #include "../inc/polynomials.h"
 
@@ -8,7 +8,9 @@ using namespace std;
 
 int main()
 {
-    testQuarticPolynomial<fp_t>(100000, 1e-5, 4, 0); // 4 кластеризированных корня
-    testQuarticPolynomial<fp_t>(100000, 1e-5, 0, 0); // 4 некластеризированных корня
-    testQuarticPolynomial<fp_t>(100000, 1e-5, 0, 4); // 4 кратных корня
+    output_equation<float>(10000, 1e-5, 4, 0);
+
+    testQuarticPolynomial<fp_t>(1000000, 1e-5, 4, 0); // 4 кластеризированных корня
+    testQuarticPolynomial<fp_t>(1000000, 1e-5, 0, 0); // 4 некластеризированных корня
+    testQuarticPolynomial<fp_t>(1000000, 1e-5, 0, 4); // 4 кратных корня
 }
